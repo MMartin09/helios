b95888ecf699e6f5aae67c62d5841776947b7e7a
MMartin09 <mmartin09@outlook.at>
2023-10-28 19:25:23 +0200

    Create endpoints for the current data

--- src/current_data/endpoints.py
+++ src/current_data/endpoints.py
@@ -1,3 +1,15 @@
-from fastapi import APIRouter
+from typing import Any
+
+from fastapi import APIRouter, Request
 
 router = APIRouter()
+
+
+@router.post("/powerflow")
+async def powerflow(request: Request) -> Any:
+    return {"status": "ok"}
+
+
+@router.post("/meter")
+async def meter(request: Request) -> Any:
+    return {"status": "ok"}
